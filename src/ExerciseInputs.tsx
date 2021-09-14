2cbbf8e78015aa4e30690b63df337a0578c63cf1
David Sanders <david.peter.sanders@googlemail.com>
2021-09-14 18:12:38 +0100

    began mocking inputs

--- src/ExerciseInputs.tsx
+++ src/ExerciseInputs.tsx
@@ -1,27 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExerciseInputs.css";
 
-const labelledInput = (name: string, type: string) => {
+interface LabelledInputProps {
+    name: string;
+    type: string;
+}
+
+const LabelledInput = (props: LabelledInputProps) => {
     return (
         <div className="labelledInput">
-            <label htmlFor={name}>{name}:</label>
-            <input type={type} id={name}></input>
+            <label htmlFor={props.name}>{props.name}:</label>
+            <input type={props.type} id={props.name}></input>
         </div>
     );
 };
 
-const ExerciseInputForm = () => {
+const SetInputForm = () => {
     return (
         <div>
             <form>
-                {labelledInput("Exercise", "text")}
-                {labelledInput("Sets", "number")}
-                {labelledInput("Reps per set", "number")}
-                {labelledInput("Weight (kg)", "number")}
-                <button>Submit</button>
+                {/* <LabelledInput name="Sets" type="number"></LabelledInput> */}
+                <LabelledInput name="Reps" type="number"></LabelledInput>
+                <LabelledInput name="Weight (kg)" type="number"></LabelledInput>
             </form>
         </div>
     );
 };
 
-export default ExerciseInputForm;
+const ExerciseInputForm = () => {
+    const emptySets: JSX.Element[] = [];
+
+    const [sets, setSets] = useState(emptySets);
+
+    const addSet = (e: React.MouseEvent) => {
+        event?.preventDefault();
+        setSets([...sets, <SetInputForm key={sets.length}></SetInputForm>]);
+    };
+
+    return (
+        <div>
+            <form>
+                <LabelledInput name="Exercise" type="text"></LabelledInput>
+                <button onClick={(e) => addSet(e)}>Add Set</button>
+            </form>
+            {sets}
+        </div>
+    );
+};
+
+const ExerciseInputFormTable = () => {
+    // TODO: see if we can replace this state with a prop (eg make it more easily configurable by input when called)
+    const emptyExercises: JSX.Element[] = [];
+
+    const [exercises, setExercises] = useState(emptyExercises);
+
+    const addExercise = (e: React.MouseEvent) => {
+        e.preventDefault();
+        setExercises([
+            ...exercises,
+            <ExerciseInputForm key={exercises.length}></ExerciseInputForm>,
+        ]);
+    };
+
+    return (
+        <div>
+            {exercises}
+            <button onClick={(e) => addExercise(e)}>Add Exercise</button>
+        </div>
+    );
+};
+
+export default ExerciseInputFormTable;
